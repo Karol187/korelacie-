a7ca4ccc885005f070b24a9a363f5aca17cb6a63
Karol187 <64404598+Karol187@users.noreply.github.com>
2020-04-28 13:08:54 +0200

    Add files via upload

---
+++ korelovanie pm10.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 13:05:04 2020
+
+@author: kocok
+"""
+
+import numpy as np
+import pandas as pd
+import netCDF4 
+import itertools
+import statistics
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from matplotlib import colors
+pd.set_option('display.max_columns',15)
+###############################
+# get netcdf4 dataset as f
+f = netCDF4.Dataset(r"C:\Users\kocok\Desktop\Bakalarska praca hlavne programy\inputs\GRIDCRO2D_2017-01-01.nc") 
+DF=pd.read_csv(r"C:\Users\kocok\Desktop\Bakalarska praca hlavne programy\inputs\tabulka_surova_PM10.csv",delimiter=',')
+#DF=pd.read_csv(r"C:\Users\kocok\Desktop\Bakalarska praca hlavne programy\inputs\tabulka_surova_NO2_2017.csv",delimiter=',')
+#########
+#get dictonary, which contain arrays of different supplementary quantities of shape (241,172), arrays are 2D
+dic_polia={}
+for i in list(f.variables.keys()):
+    if i not in ['MSFX2','TFLAG','LWMASK', 'LAT', 'LON']: 
+    #if i not in ['MSFX2','TFLAG','LWMASK', 'LAT', 'LON','LUFRAC_11','LUFRAC_07']:
+    #if i in ['HT']:
+       dic_polia[i]=np.array(f.variables[i][0,0,:,:])
+dic_polia['TOTALPM10']=np.float32(np.load(r"C:\Users\kocok\Desktop\Bakalarska praca hlavne programy\inputs\total_PM10.npy"))
+#dic_polia['TOTALNO2']=np.float32(np.load(r"C:\Users\kocok\Desktop\Bakalarska praca hlavne programy\inputs\total_NO2.npy"))
+dic_polia['HUSTOTA_OBYVATELSTVA']=np.float32(np.load(r"C:\Users\kocok\Desktop\Bakalarska praca hlavne programy\inputs\HUSTOTA_OBYVATELSTVA.npy"))
+dic_polia['mapa_pm10_v6']=np.float32(np.load(r"C:\Users\kocok\Desktop\Bakalarska praca hlavne programy\inputs\mapa_pm10_v6.npy"))
+########
+# get disctonary, which contains LAT an LON arrays of shape (241,172), arrays are 2D
+dic_latlon={}
+for i in list(f.variables.keys()):
+    if i in ['LAT','LON']:
+       dic_latlon[i]=np.array(f.variables[i][0,0,:,:])
+################################################################
+def getclosest_ij(lats,lons,latpt,lonpt):
+    # find squared distance of every point on grid
+    dist_sq = (lats-latpt)**2 + (lons-lonpt)**2  
+    # 1D index of minimum dist_sq element
+    minindex_flattened = dist_sq.argmin()    
+    # Get 2D index for latvals and lonvals arrays from 1D index
+    return np.unravel_index(minindex_flattened, lats.shape)
+################################################################   
+
+    
+for key,value in dic_polia.items():
+    #DF=pd.read_csv('tabulka_surova_PM10.csv',delimiter=',')
+    zoznam_vsetky=[]
+    zoznam_UB_RB_SB_SI=[]
+    zoznam_UT_RB_SB_SI=[]
+    zoznam_UB_UT_SB_SI=[]
+    zoznam_UB_UT_RB_SB=[]
+    zoznam_UB_UT_RB_SI=[]
+    zoznam_UB=[]
+    zoznam_UT=[]
+    for n in range (0,4):
+        z=[]
+        for k, row in DF.iterrows():
+            ix, iy = getclosest_ij(dic_latlon['LAT'],dic_latlon['LON'],row['lat_x'],row['lon_x'])
+            listname=[]
+        
+            i=list(range(ix-n,ix+n+1,1))
+            j=list(range(iy-n,iy+n+1,1))
+            for each in list(itertools.product(i,j)):
+                listname.append(value[each])
+            pd.Series(listname).fillna(0).tolist()
+            upraveny_list=pd.Series(listname,dtype=object).fillna(0).tolist()
+            z.append(sum(upraveny_list))
+##################################################################################################
+#KORELACIE S ln(PM10)
+##################################################################################################              
+#        DF[key]=z
+#        korelacia_vsetky=np.log(DF['PM10']).corr(DF[key])
+#        zoznam_vsetky.append(korelacia_vsetky)
+#        DF_filteredUT = DF[(DF.typ != 'UT')]
+#        korelacia_UB_RB_SB_SI=np.log(DF_filteredUT['PM10']).corr(DF_filteredUT[key])
+#        zoznam_UB_RB_SB_SI.append(korelacia_UB_RB_SB_SI)
+#        DF_filteredUB = DF[(DF.typ != 'UB')]
+#        korelacia_UT_RB_SB_SI=np.log(DF_filteredUB['PM10']).corr(DF_filteredUB[key])
+#        zoznam_UT_RB_SB_SI.append(korelacia_UT_RB_SB_SI)
+#        DF_filteredRB = DF[(DF.typ != 'RB')]
+#        korelacia_UB_UT_SB_SI=np.log(DF_filteredRB['PM10']).corr(DF_filteredRB[key])
+#        zoznam_UB_UT_SB_SI.append(korelacia_UB_UT_SB_SI)
+#        DF_filteredSI = DF[(DF.typ != 'SI')]
+#        korelacia_UB_UT_RB_SB=np.log(DF_filteredSI['PM10']).corr(DF_filteredSI[key])
+#        zoznam_UB_UT_RB_SB.append(korelacia_UB_UT_RB_SB)
+#        DF_filteredSB = DF[(DF.typ != 'SB')]
+#        korelacia_UB_UT_RB_SI=np.log(DF_filteredSB['PM10']).corr(DF_filteredSB[key])
+#        zoznam_UB_UT_RB_SI.append(korelacia_UB_UT_RB_SI)
+#        DF_filter = DF[(DF.typ != 'SI')& (DF.typ != 'SB')& (DF.typ != 'RB')& (DF.typ != 'UT')]
+#        korelacia_UB = np.log(DF_filter['PM10']).corr(DF_filter[key])
+#        zoznam_UB.append(korelacia_UB)
+#        DF_filter1 = DF[(DF.typ != 'SI')& (DF.typ != 'SB')& (DF.typ != 'RB')& (DF.typ != 'UB')]
+#        korelacia_UT=np.log(DF_filter1['PM10']).corr(DF_filter1[key])
+#        zoznam_UT.append(korelacia_UT)       
+##################################################################################################
+#KORELACIE S PM10
+##################################################################################################
+        DF[key]=z
+        korelacia_vsetky=DF['PM10'].corr(DF[key])
+        zoznam_vsetky.append(korelacia_vsetky)
+        DF_filteredUT = DF[(DF.typ != 'UT')]
+        korelacia_UB_RB_SB_SI=DF_filteredUT['PM10'].corr(DF_filteredUT[key])
+        zoznam_UB_RB_SB_SI.append(korelacia_UB_RB_SB_SI)
+        DF_filteredUB = DF[(DF.typ != 'UB')]
+        korelacia_UT_RB_SB_SI=DF_filteredUB['PM10'].corr(DF_filteredUB[key])
+        zoznam_UT_RB_SB_SI.append(korelacia_UT_RB_SB_SI)
+        DF_filteredRB = DF[(DF.typ != 'RB')]
+        korelacia_UB_UT_SB_SI=DF_filteredRB['PM10'].corr(DF_filteredRB[key])
+        zoznam_UB_UT_SB_SI.append(korelacia_UB_UT_SB_SI)
+        DF_filteredSI = DF[(DF.typ != 'SI')]
+        korelacia_UB_UT_RB_SB=DF_filteredSI['PM10'].corr(DF_filteredSI[key])
+        zoznam_UB_UT_RB_SB.append(korelacia_UB_UT_RB_SB)
+        DF_filteredSB = DF[(DF.typ != 'SB')]
+        korelacia_UB_UT_RB_SI=DF_filteredSB['PM10'].corr(DF_filteredSB[key])
+        zoznam_UB_UT_RB_SI.append(korelacia_UB_UT_RB_SI)
+        DF_filter = DF[(DF.typ != 'SI')& (DF.typ != 'SB')& (DF.typ != 'RB')& (DF.typ != 'UT')]
+        korelacia_UB = DF_filter['PM10'].corr(DF_filter[key])
+        zoznam_UB.append(korelacia_UB)
+        DF_filter1 = DF[(DF.typ != 'SI')& (DF.typ != 'SB')& (DF.typ != 'RB')& (DF.typ != 'UB')]
+        korelacia_UT=DF_filter1['PM10'].corr(DF_filter1[key])
+        zoznam_UT.append(korelacia_UT)       
+
+################################################################################################
+#OBLAST KORELACIE (GRID BOD ALEBO VIAC)       
+################################################################################################        
+    plocha=[]
+    for oblast in np.arange(1.5,13.5,3):
+            plocha.append(oblast*oblast)
+    tabulka=pd.DataFrame({'oblast km^2': plocha,'Vsetky':zoznam_vsetky, 'UB_RB_SB_SI':zoznam_UB_RB_SB_SI, 'UT_RB_SB_SI':zoznam_UT_RB_SB_SI,'UB_UT_SB_SI':zoznam_UB_UT_SB_SI, 'UB_UT_RB_SB':zoznam_UB_UT_RB_SB,'UB_UT_RB_SI':zoznam_UB_UT_RB_SI, 'zoznam_UB':zoznam_UB,'zoznam_UT':zoznam_UT})
+    
+    tabulka_zaokruhlena=tabulka.round({'Vsetky': 3, 'UB_RB_SB_SI': 3,'UT_RB_SB_SI':3,'UB_UT_SB_SI':3,'UB_UT_RB_SB':3,'UB_UT_RB_SI':3,'zoznam_UB':3,'zoznam_UT':3})
+    #mala tabulka 
+    primerna_korelacia_vsetky=statistics.mean(tabulka['Vsetky'])
+    primerna_korelacia_UB_RB_SB_SI=statistics.mean(tabulka['UB_RB_SB_SI'])
+    primerna_korelacia_UT_RB_SB_SI=statistics.mean(tabulka['UT_RB_SB_SI'])
+    primerna_korelacia_UB_UT_SB_SI=statistics.mean(tabulka['UB_UT_SB_SI'])
+    primerna_korelacia_UB_UT_RB_SB=statistics.mean(tabulka['UB_UT_RB_SB'])
+    primerna_korelacia_UB_UT_RB_SI=statistics.mean(tabulka['UB_UT_RB_SI'])
+    primerna_korelacia_zoznam_UB=statistics.mean(tabulka['zoznam_UB'])
+    primerna_korelacia_zoznam_UT=statistics.mean(tabulka['zoznam_UT'])
+    
+    # initialize list of lists 
+    data = [['Vsetky',primerna_korelacia_vsetky ], ['UB_RB_SB_SI', primerna_korelacia_UB_RB_SB_SI], ['UT_RB_SB_SI', primerna_korelacia_UT_RB_SB_SI],['UB_UT_SB_SI', primerna_korelacia_UB_UT_SB_SI], ['UB_UT_RB_SB', primerna_korelacia_UB_UT_RB_SB], ['UB_UT_RB_SI', primerna_korelacia_UB_UT_RB_SI],['zoznam_UB', primerna_korelacia_zoznam_UB], ['zoznam_UT', primerna_korelacia_zoznam_UT]]  
+    # Create the pandas DataFrame 
+    df = pd.DataFrame(data, columns = ['Zahrnuté typy staníc ', 'Priemerná korelácia'])   
+   
+    print("##################################")
+    print(key)
+    print("##################################")
+    print(tabulka_zaokruhlena)
+    print(df)
+    dfzaokruhlene=df.round({'Priemerná korelácia': 3})
+    print(dfzaokruhlene)
+    print("##################################")
+################################################################################################
+#ZOBRAZENIE POLA NA MAPE     
+################################################################################################          
+    
+    plt.rcParams['figure.figsize'] =20,7
+    meridians = np.arange(16.,35.,1.)
+    pararels = np.arange(46.,50.,1.)
+    mapb=Basemap(projection='lcc',lat_1=48,lat_2=48,lat_0=48.7,lon_0=19.7,width=500000,height=300000,resolution='i')
+    mapb.drawcountries()
+    mapb.pcolormesh(dic_latlon['LON'],dic_latlon['LAT'],value,cmap=plt.cm.jet,latlon=True) 
+    # drawing locations of stations in maps
+    #x,y = mapb(list(DF['lon_x']), list(DF['lat_x']))
+    #mapb.plot(x, y, 'bo', markersize=4)
+    plt.colorbar(label= key)
+    plt.title(key,fontsize=15)
+    plt.show() 
+    
+    print("##################################")
+    
